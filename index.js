8d9118b42118d813c9c73abac245bc123bbfb2bf
Thomas Ermesjo <thomas.ermesjo@schibsted.no>
2016-01-26 09:56:10 +0100

    .

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var mongoRepository = require('./stockRepository')();
 var app = require('./app')(mongoRepository);
 var port = process.env.PORT || 3000;
 
-app.listen(3000, function () {
+app.listen(port, function () {
     console.log('Example app listening on port 3000!');
 });
